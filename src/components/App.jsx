28d62b78d1efdaf52f4870f9a305853575a7d13d
gajendra93 <gajendrasp.rj.9693@gmail.com>
2016-12-07 15:41:26 +0530

    Folder structure created

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Header from './Header.jsx';
+import Footer from './Footer.jsx';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <Header />
+                <div className="content">
+                    {this.props.children}
+                </div>
+                <Footer />
+            </div>
+        )
+    }
+}
+
+export default App;
---
+++ src/components/ContactUs.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class ContactUs extends React.Component {
+      render() {
+        return (
+            <div>
+                <h1>Contact Us Details:</h1>
+                <pre>
+                	Address Line 1
+                	Address Line 2
+                	City
+                	M. No.
+                </pre>
+            </div>
+        )
+    }
+}
+
+export default ContactUs;
