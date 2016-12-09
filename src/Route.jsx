a607a7d487c95285871d167e111e996ec154f77e
RavinaBansal <ravina.1706@gmail.com>
2016-12-09 09:45:17 +0530

    Naming convention
    correct naming convention of the components.

--- src/Route.jsx
+++ src/Route.jsx
@@ -4,10 +4,9 @@ import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
 import Home from './components/Home.jsx';
 import SearchMovies from './components/SearchMovies.jsx';
-import CardExampleWithAvatar from './components/MovieDetail.jsx';
+import MovieDetail from './components/MovieDetail.jsx';
 import ContactUs from './components/ContactUs.jsx';
 import NotFound from './components/NotFound.jsx';
-import PaperExampleSimple from './components/papercomponent.jsx';
 import {Router, Route, IndexRoute, hashHistory} from 'react-router';
 
 var destination = document.querySelector("#root");
@@ -17,7 +16,7 @@ ReactDOM.render(
 	    <Route path="/" component={App}>
 		    <IndexRoute component={Home} />
 		    <Route path="search" component={SearchMovies} />
-		    <Route path="detail" component={CardExampleWithAvatar} />
+		    <Route path="detail" component={MovieDetail} />
 		    <Route path="contact" component={ContactUs} />
 		    <Route path='*' component={NotFound} />
 	    </Route>
